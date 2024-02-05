203a4976a3ec585bc4917be75c6309be88822e49
Lucas Lemes Borges <borgeslucaslemes@gamil.com>
2024-02-05 17:48:36 -0300

    chore:Home page header on mobile

---
+++ src/app/fonts.ts
@@ -0,0 +1,5 @@
+import { Gwendolyn,Hubballi } from "next/font/google";
+
+export const gwendolyn = Gwendolyn({ subsets: ["latin"], weight: ["400", "700"]}) 
+
+export const hubballi = Hubballi({ subsets: ["latin"], weight: "400"})
