b86c169308f1e39c773d026dd42e2a6bdc11f515
Lucas Lemes Borges <borgeslucaslemes@gamil.com>
2024-02-05 21:48:49 -0300

    chore: changes to the header structure on the mobile home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,9 @@ export default function Home() {
   return (
     <div className="p-[5rem_1rem_0.5rem]">
       <h1 className="font-bold text-4xl font-serif text-center">Lucas L. Borges</h1>
-      <div className="w-fit mx-auto uppercase tracking-12 mt-16 mb-24">
-
-        <h2 className={`${gwendolyn.className} inline text-3xl w-fit pl-8 mr-3 `}>ls</h2>
-        <p className="inline">O bom humor é a unica</p>
+      <div className="w-fit mx-auto uppercase tracking-12 mt-16 mb-24 text-center">
+        <h2 className={`${gwendolyn.className} text-3xl font-bold mb-1`}>ls</h2>
+        <p className="mb-1">O bom humor é a unica</p>
         <p className="">qualidade divina do homem</p>
       </div>
     </div>
