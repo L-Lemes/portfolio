e366dde320ff0d75933ff5f04c2ee44a885ffdda
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-02-13 09:17:39 -0300

    styles: stylized navbar in mobile, sm and md versions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
   return (
     <div className="relative border-y-[1px] border-black w-full before:absolute before:-top-[0.25rem] before:block before:w-full before:h-[0.75px] before:bg-black lg:before:-top-[0.375rem]">
-      <ul className="flex justify-between px-6 pb-3 pt-[1.125rem] uppercase text-sm md:text-base  xl:text-lg">
+      <ul className="flex justify-between px-6 pb-3 pt-[1.125rem] uppercase text-sm md:text-base md:px-10 xl:text-lg xl:px-12">
         <li>
           <a href="/about">about</a>
         </li>
@@ -14,7 +14,7 @@ export const Navbar = () => {
         <li>
           <a href="/blog">blog</a>
         </li>
-        <li className="hidden xl:inline">
+        <li className="hidden lg:inline">
           <a href="/blog">Experience</a>
         </li>
       </ul>
