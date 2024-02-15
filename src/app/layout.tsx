f41fe83cdab79e49b36664ada28bdb953a2bd03f
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-02-15 16:45:28 -0300

    chore: added a custom metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { hubballi } from "./fonts";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "",
+  title: "LS dev blog",
   description: "Portfolio de desenvolvedor idealizado e desenvolvido por Lucas Lemes Borges",
   icons: {
     icon: '/icon.png'
