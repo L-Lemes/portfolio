3cfaae4688475f373762e71f5c356e4596c5bf4c
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-02-18 08:52:16 -0300

    styles: home page complete

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 export const ProjectCard = () => {
   return (
     <div className="flex justify-center items-center flex-1 w-full h-full mt-8 bg-featured-image bg-cover bg-center md:mt-12 lg:mt-8 xl:mt-10">
-      <div className="text-center bg-amber-50 w-[50%] xl:h-40 box-content px-6 pb-8 pt-10 sm:box-border md:w-[40%] md:pb-10 md:pt-12 lg:w-[30%] lg:pb-8 xl:w-[25%] xl:pb-12">
-        <p className="text-2xl md:text-3xl md:mb-1 xl:text-4xl">VPN Project</p>
+      <div className="text-center bg-amber-50 w-[50%] xl:h-44 box-content px-6 pb-8 pt-10 sm:box-border md:w-[40%] md:pb-10 md:pt-12 lg:w-[30%] lg:pb-8 xl:w-[25%] xl:pb-12">
+        <p className="text-2xl md:text-3xl md:mb-1 xl:text-4xl xl:mb-4">VPN Project</p>
         <Link
           href="/projects"
           className="relative text-nowrap text-sm text-[#666] sm:text-base md:text-lg xl:text-xl
