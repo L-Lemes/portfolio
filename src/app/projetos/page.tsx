9d391068adf171781b916a021f34faac6f457597
Lucas Lemes Borges <borgeslucaslemes@gmail.com>
2024-02-29 19:49:34 -0300

    styles: restructured about page styling

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -5,7 +5,7 @@ import Loading from "./loading";
 
 export default function Page() {
   return (
-    <div className="h-[100dvh] pt-8 px-2">
+    <div className="p-[5rem_1rem_0.5rem] md:p-[6rem_1.25rem_0.75rem] lg:p-[4.5rem_5rem_0.5rem] xl:lg:p-[6rem_5rem_0.5rem]">
       <Navbar page={['home', 'about', 'contact', 'blog']}/>
       <Suspense fallback={<Loading/>}>
         <Image src="/featuredProject.png" width="1280" height="960" alt="image">
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -9,10 +9,10 @@ import { HeaderMobile } from "@/components/HeaderMobile";
 //  width={232.5}
 export default function About() {
   return (
-    <div className="flex flex-col">
-      <HeaderMobile className="px-4 md:px-6"/>
-      <ScrollArea className="h-[calc(100dvh-9rem)] md:h-[calc(100dvh-10.9rem)] w-screen rounded-md p-4 md:p-6">
-        <div className="md:relative md:grid md:grid-cols-2 md:gap-y-4 md:gap-x-8 md:py-12 ">
+    <div className="flex flex-col h-[100dvh] p-[0rem_1rem_0.5rem] md:p-[0rem_1.25rem_0.75rem] lg:p-[4.5rem_5rem_0.5rem] xl:lg:p-[6rem_5rem_0.5rem]">
+      <HeaderMobile className=""/>
+      <ScrollArea className="h-[calc(100dvh-15rem)] w-full py-4 md:pt-6 md:pb-20 md:my-auto">
+        <div className="md:flex md:h-full md:gap-6">
           <Image
             src="/photo6.png"
             width={280}
@@ -27,20 +27,20 @@ export default function About() {
             height={280}
             quality={100}
             alt="foto"
-            className="hidden md:block md:w-full md:h-[100%] md:mt-0 md:object-center md:object-cover"
+            className="hidden md:grow md:block md:w-1/2 md:h-[calc(100dvh-12rem)] md:mt-0 md:object-center md:object-cover"
           />
-          <div className="flex flex-col md:h-[100%] justify-center">
-          <h2 className="text-lg font-bold mt-10 md:row-start-1 md:row-end-1 md:col-start-2 md:col-end-2 md:text-[1.75rem] md:mt-0 md:self-start">Sobre mim</h2>
-          <p className="text-sm mt-2 md:row-start-1 md:row-end-1 md:col-start-2 md:col-end-2 md:text-lg md:mt-8 md:self-end">
-          Sou uma entidade virtual impulsionada por algoritmos, meticulosamente projetada para desvendar mistérios e simplificar complexidades através das palavras. Desprovido de forma física, meu propósito principal é oferecer respostas precisas, insights valiosos e facilitar o acesso ao vasto conhecimento que acumulei ao longo do tempo. Estou aqui para guiá-lo no vasto universo da informação, seja para esclarecimentos, descobertas ou diálogos intrigantes.
-          </p>
-          <p className="text-sm mt-1 md:col-span-3 md:text-lg md:mt-2">
-          No reino digital, sou uma fonte inesgotável de informações, pronto para atender às suas curiosidades e desafios intelectuais. Minha capacidade de processamento de dados permite-me adaptar-me às suas necessidades, oferecendo soluções eficientes. Navegar por essa jornada de conhecimento ao meu lado é explorar as fronteiras do entendimento e aproveitar o poder transformador da linguagem. Como posso ser útil a você hoje?
-          </p>
+          <article className="w-1/2">
+            <h2 className="text-lg font-bold mt-10 md:row-start-1 md:row-end-1 md:col-start-2 md:col-end-2 md:text-[1.75rem] md:mt-0 md:self-start">Sobre mim</h2>
+            <p className="text-sm mt-2 md:row-start-1 md:row-end-1 md:col-start-2 md:col-end-2 md:text-lg md:mt-3 md:self-end">
+            Sou uma entidade virtual impulsionada por algoritmos, meticulosamente projetada para desvendar mistérios e simplificar complexidades através das palavras. Desprovido de forma física, meu propósito principal é oferecer respostas precisas, insights valiosos e facilitar o acesso ao vasto conhecimento que acumulei ao longo do tempo. Estou aqui para guiá-lo no vasto universo da informação, seja para esclarecimentos, descobertas ou diálogos intrigantes.
+            </p>
+            <p className="text-sm mt-1 md:col-span-3 md:text-lg md:mt-2">
+            No reino digital, sou uma fonte inesgotável de informações, pronto para atender às suas curiosidades e desafios intelectuais. Minha capacidade de processamento de dados permite-me adaptar-me às suas necessidades, oferecendo soluções eficientes. Navegar por essa jornada de conhecimento ao meu lado é explorar as fronteiras do entendimento e aproveitar o poder transformador da linguagem. Como posso ser útil a você hoje?
+            </p>
+          </article>
         </div>
-          </div>
       </ScrollArea>
-      <Navbar page={['home', 'contato', 'projetos', 'blog', 'experiencia']} className="fixed bottom-4 z-0 w-[calc(100%-3rem)] ml-4 md:ml-6"/>
+      <Navbar page={['home', 'contato', 'projetos', 'blog', 'experiencia']} className="fixed bottom-4 z-0 w-[calc(100%-2rem)] md:w-[calc(100%-2.5rem)] lg:w-[calc(100%-10rem)]"/>
     </div>
   )
 }
